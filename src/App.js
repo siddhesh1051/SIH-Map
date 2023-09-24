9f21a92e18bbca0d509edcd2105e41fdbfe754bd
siddhesh1051 <siddhesh1052001@gmail.com>
2023-09-25 00:13:32 +0530

    Added Vertical

--- src/App.js
+++ src/App.js
@@ -81,70 +81,10 @@ function App() {
   }
 
   return (
-    <div className="App" style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <div style={{ marginTop: "5px" }}>
-        <label
-          htmlFor="city-select"
-          style={{
-            marginRight: "10px",
-            fontSize: "18px",
-            fontWeight: "bold",
-            color: "#333",
-          }}
-        >
-          Select a city:
-        </label>
-        <select
-          id="city-select"
-          value={city}
-          onChange={handleCityChange}
-          style={{
-            padding: "10px",
-            fontSize: "16px",
-            borderRadius: "5px",
-            border: "2px solid #f4D7f7",
-            backgroundColor: "#f7f7f7",
-            color: "#333",
-            cursor: "pointer",
-            outline: "none",
-          }}
-        >
-          <option value="Nagpur" >Nagpur</option>
-          <option value="Hamirpur">Hamirpur</option>
-          <option value="Una">Una</option>
-          <option value="Imphal">Imphal</option>
-          <option value="Pune">Pune</option>
-          <option value="Jaipur">Jaipur</option>
-        </select>
-      </div>
 
-      <Map
-        ref={mapRef}
-        mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
-        style={{
-          width: "90vw",
-          height: "80vh",
-          borderRadius: "15px",
-          marginTop: "20px",
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      >
-        {
-          cityBuses.map((bus) => (
-            <Marker key={bus.index} longitude={bus.lng} latitude={bus.lat}>
-              <img
-                src={selectedBus === bus ? busIcon2 : busIcon} // Use busIcon2 when selected
-                alt="Bus Marker"
-                style={{ width: "40px", height: "40px", cursor: "pointer" }}
-                onClick={() => handleBusClick(bus)} // Handle click event
-              />
-            </Marker>
-          ))
-        }
-        <NavigationControl position="bottom-right" />
-        <FullscreenControl />
-        <GeolocateControl />
-      </Map>
+
+    <div className="App" style={{ display: "flex", justifyContent: 'center', alignItems: 'start' }}>
+
       {/* <div style={{ marginTop: "10px" }}>
         <Timeline/>
         </div> */}
@@ -153,7 +93,7 @@ function App() {
 
 
 
-        <VerticalTimeline className="mt-16" lineColor={ 'black' } layout="1-column-left" >
+        <VerticalTimeline className="mt-16" lineColor={'black'} layout="1-column-left" >
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
@@ -238,6 +178,72 @@ function App() {
             iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
           />
         </VerticalTimeline>
+      </div>
+      <div className="flex flex-col justify-start items-center ">
+
+        <div style={{ marginTop: "5px" }}>
+          <label
+            htmlFor="city-select"
+            style={{
+              marginRight: "10px",
+              fontSize: "18px",
+              fontWeight: "bold",
+              color: "#333",
+            }}
+          >
+            Select a city:
+          </label>
+          <select
+            id="city-select"
+            value={city}
+            onChange={handleCityChange}
+            style={{
+              padding: "10px",
+              fontSize: "16px",
+              borderRadius: "5px",
+              border: "2px solid #f4D7f7",
+              backgroundColor: "#f7f7f7",
+              color: "#333",
+              cursor: "pointer",
+              outline: "none",
+            }}
+          >
+            <option value="Nagpur" >Nagpur</option>
+            <option value="Hamirpur">Hamirpur</option>
+            <option value="Una">Una</option>
+            <option value="Imphal">Imphal</option>
+            <option value="Pune">Pune</option>
+            <option value="Jaipur">Jaipur</option>
+          </select>
+        </div>
+
+        <Map
+          ref={mapRef}
+          mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
+          style={{
+            width: "50vw",
+            height: "80vh",
+            borderRadius: "15px",
+            marginTop: "20px",
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          {
+            cityBuses.map((bus) => (
+              <Marker key={bus.index} longitude={bus.lng} latitude={bus.lat}>
+                <img
+                  src={selectedBus === bus ? busIcon2 : busIcon} // Use busIcon2 when selected
+                  alt="Bus Marker"
+                  style={{ width: "40px", height: "40px", cursor: "pointer" }}
+                  onClick={() => handleBusClick(bus)} // Handle click event
+                />
+              </Marker>
+            ))
+          }
+          <NavigationControl position="bottom-right" />
+          <FullscreenControl />
+          <GeolocateControl />
+        </Map>
       </div>
     </div>
   );
